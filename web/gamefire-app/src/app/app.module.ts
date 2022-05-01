d99900e086ffefc7874ef7060dbb5a468514e10c
Jonay Hernández Izquierdo <jonayhi1717@gmail.com>
2022-05-01 11:51:04 +0100

    feat: add simple navbar and final footer

--- web/gamefire-app/src/app/app.module.ts
+++ web/gamefire-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
